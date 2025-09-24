860b5810abaa24a5d568d63784ade236a83395cf
Chirani Rajapaksha <chirani.rajapaksha.97@gmail.com>
2025-09-24 11:07:03 +0530

    Memory management

--- Theory/MemoryManagement.cpp
+++ Theory/MemoryManagement.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
 
+void swapByValue(int a, int b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+void swapByRef(int& a, int& b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void swapByPointer(int* a, int* b) //Discouraged
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+class Student{
+    private:
+        int age;
+    public:
+        Student () : age(12) {} //default constructor
+        int getAge()
+        {
+            return age;
+        }
+};
+
 int main(){
     //Address
     std::cout << "\n=================Address=================\n";
@@ -24,6 +58,51 @@ int main(){
     std::string& ref = city;
     std::cout << "Variable Value: " << city << std::endl;
     std::cout << "Reference Value: " << ref << std::endl;
+
+    //Pass by Reference
+    std::cout << "\n=================Pass By Reference=================\n";
+    int x,b,c,d,e,f;
+    x = 1;
+    b = 1;
+    c = 2;
+    d = 2;
+    e = 3;
+    f = 4;
+    std::cout << x << b << c << d << e << f << std::endl;
+    swapByValue(x,c); //does not swap
+    swapByRef(b, d); // swaps
+    swapByPointer(&e,&f); //swaps but not in use
+    std::cout << x << b << c << d << e << f << std::endl;
+    
+    //Memory Management
+    std::cout << "\n=================Memory Management=================\n";
+    //Dynamic memory allocation
+    int* point_int = new int{6};
+    float* point_float = new float{2.2};
+    std::cout << "point_int: " << point_int << " point_float: " << point_float << std::endl;
+    std::cout << "point_int value: " << *point_int << " point_float value: " << *point_float << std::endl;
+    delete point_int;
+    delete point_float;
+    std::cout << "--After delete--\n";
+    std::cout << "point_int: " << point_int << " point_float: " << point_float << std::endl; //still has memory location
+    std::cout << "point_int value: " << *point_int << " point_float value: " << *point_float << std::endl; //Might show garbage
+    //Safe dereferencing
+    point_int = nullptr;  
+    point_float = nullptr; 
+    std::cout << "--After Safe dereferencing--\n";
+    std::cout << "point_int: " << point_int << " point_float: " << point_float << std::endl; //no more memory is allocated
+    //SO no more value or garbage value to access
+
+    //For arrays
+    float* marks = new float[]{30, 66.7, 98, 45};
+    delete[] marks;
+    marks = nullptr;
+
+    //For objects - Student class
+    Student* point_student = new Student(); //Default constructor is called
+    std::cout << "Age: " << point_student->getAge() << std::endl;
+    delete point_student;
+    point_student = nullptr;
 
     return 0;
 }
